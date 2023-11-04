352056a6027f4274cc1c7657c5da9e43c0fed4f5
michellenikeetha <michellenikeetha@gmail.com>
2023-11-04 23:27:24 +0530

    micelle initial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from 'react'
 import './App.css'
 import Sidebar from './common/Sidebar'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+import login from './components/login'
 
-  return (
-    <>
-      <Sidebar />
-    </>
-  )
-}
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <Sidebar />
+//     </>
+//   )
+// }
+
+const App =() => (
+  <Router>
+    <Routes>
+      <Route path="/login" element={<login/>} />
+      <Route path ="/" element={<Sidebar/>} />
+    </Routes>
+  </Router>  
+)
 
 export default App
