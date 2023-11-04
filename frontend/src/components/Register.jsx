59f00cf15bbd6a5b4e0926025e891df5224c860b
michellenikeetha <michellenikeetha@gmail.com>
2023-11-05 02:11:46 +0530

    some changes by michelle

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -24,14 +24,23 @@ export default function Register() {
                     <label htmlFor="firstName" className="text-xs font-semibold px-1">First name</label>
                     <div className="flex">
                       <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                      <input type="text" id="firstName" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus-border-indigo-500" placeholder="John" />
+                      <input type="text" id="firstName" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="John" />
                     </div>
                   </div>
                   <div className="w-1/2 px-3 mb-5">
                     <label htmlFor="lastName" className="text-xs font-semibold px-1">Last name</label>
                     <div className="flex">
                       <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                      <input type="text" id="lastName" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus-border-indigo-500" placeholder="Smith" />
+                      <input type="text" id="lastName" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Smith" />
+                    </div>
+                  </div>
+                </div>
+                <div className="flex -mx-3">
+                  <div className="w-full px-3 mb-5">
+                    <label htmlFor="phone" className="text-xs font-semibold px-1">Mobile Phone</label>
+                    <div className="flex">
+                      <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
+                      <input type="tel" id="tel" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="07x xxxx xxx" />
                     </div>
                   </div>
                 </div>
@@ -40,7 +49,7 @@ export default function Register() {
                     <label htmlFor="email" className="text-xs font-semibold px-1">Email</label>
                     <div className="flex">
                       <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
-                      <input type="email" id="email" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus-border-indigo-500" placeholder="johnsmith@example.com" />
+                      <input type="email" id="email" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="johnsmith@example.com" />
                     </div>
                   </div>
                 </div>
@@ -49,13 +58,13 @@ export default function Register() {
                     <label htmlFor="password" className="text-xs font-semibold px-1">Password</label>
                     <div className="flex">
                       <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
-                      <input type="password" id="password" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus-border-indigo-500" placeholder="************" />
+                      <input type="password" id="password" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="************" />
                     </div>
                   </div>
                 </div>
                 <div className="flex -mx-3">
                   <div className="w-full px-3 mb-5">
-                    <button className="block w-full max-w-xs mx-auto bg-indigo-500 hover-bg-indigo-700 focus-bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">REGISTER NOW</button>
+                    <button className="block w-full max-w-xs mx-auto bg-indigo-500 hover-bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">REGISTER NOW</button>
                   </div>
                 </div>
                 <p className="text-center text-gray-500">
--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -25,7 +25,7 @@ export default function Login() {
                       <label htmlFor="email" className="text-xs font-semibold px-1">Email</label>
                       <div className="flex">
                         <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
-                        <input type="email" id="email" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus-border-indigo-500" placeholder="johnsmith@example.com" />
+                        <input type="email" id="email" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="johnsmith@example.com" />
                       </div>
                     </div>
                   </div>
@@ -34,7 +34,7 @@ export default function Login() {
                       <label htmlFor="password" className="text-xs font-semibold px-1">Password</label>
                       <div className="flex">
                         <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
-                        <input type="password" id="password" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus-border-indigo-500" placeholder="************" />
+                        <input type="password" id="password" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="************" />
                       </div>
                     </div>
                   </div>
