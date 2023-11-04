bc8659b7943d29536ce2449e2dacfa39e43b6764
michellenikeetha <michellenikeetha@gmail.com>
2023-11-05 01:47:00 +0530

    final login register ui by michelle

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import register from '../assets/register.jpg'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import register from '../assets/register.jpg';
 
 export default function Register() {
   return (
     <div>
-      <div className="min-w-screen min-h-screen bg-gray-900 flex items-center justify-center px-5 py-5">
-        <div className="bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full overflow-hidden" style={{ maxWidth: '1000px' }}>
+      <div className="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center px-5 py-5">
+        <div className="bg-indigo-200 text-gray-900 rounded-3xl shadow-xl w-full overflow-hidden" style={{ maxWidth: '1000px' }}>
           <div className="md:flex w-full">
             <div className="hidden md:block w-1/2 bg-indigo-500 py-10 px-10">
-              <img src={register} style={{ height: '100%', width: '100%' }}/>
+              <img src={register} style={{ height: '100%', width: '100%' }} />
             </div>
             <div className="w-full md:w-1/2 py-10 px-5 md:px-10">
               <form>
@@ -22,14 +23,14 @@ export default function Register() {
                       <label htmlFor="firstName" className="text-xs font-semibold px-1">First name</label>
                       <div className="flex">
                         <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                        <input type="text" id="firstName" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="John" />
+                        <input type="text" id="firstName" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus-border-indigo-500" placeholder="John" />
                       </div>
                     </div>
                     <div className="w-1/2 px-3 mb-5">
                       <label htmlFor="lastName" className="text-xs font-semibold px-1">Last name</label>
                       <div className="flex">
                         <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                        <input type="text" id="lastName" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Smith" />
+                        <input type="text" id="lastName" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus-border-indigo-500" placeholder="Smith" />
                       </div>
                     </div>
                   </div>
@@ -38,7 +39,7 @@ export default function Register() {
                       <label htmlFor="email" className="text-xs font-semibold px-1">Email</label>
                       <div className="flex">
                         <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
-                        <input type="email" id="email" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="johnsmith@example.com" />
+                        <input type="email" id="email" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus-border-indigo-500" placeholder="johnsmith@example.com" />
                       </div>
                     </div>
                   </div>
@@ -47,7 +48,7 @@ export default function Register() {
                       <label htmlFor="password" className="text-xs font-semibold px-1">Password</label>
                       <div className="flex">
                         <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
-                        <input type="password" id="password" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="************" />
+                        <input type="password" id="password" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus-border-indigo-500" placeholder="************" />
                       </div>
                     </div>
                   </div>
@@ -56,6 +57,9 @@ export default function Register() {
                       <button className="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">REGISTER NOW</button>
                     </div>
                   </div>
+                  <p className="text-center text-gray-500">
+                    Already have an account? <Link to="/login" className="text-indigo-600">Login here</Link>
+                  </p>
                 </div>
               </form>
             </div>
@@ -63,5 +67,5 @@ export default function Register() {
         </div>
       </div>
     </div>
-  )
+  );
 }
--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import login from '../assets/login.jpg'
+import { Link } from 'react-router-dom'; 
+import login from '../assets/login.jpg';
 
 export default function Login() {
   return (
     <div>
-      <div className="min-w-screen min-h-screen bg-gray-900 flex items-center justify-center px-5 py-5">
-        <div className="bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full overflow-hidden" style={{ maxWidth: '1000px' }}>
+      <div className="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center px-5 py-5">
+        <div className="bg-indigo-200 text-gray-900 rounded-3xl shadow-xl w-full overflow-hidden" style={{ maxWidth: '1000px' }}>
           <div className="md:flex w-full">
             <div className="hidden md:block w-1/2 bg-indigo-500 py-10 px-10">
-              <img src={login} style={{ height: '100%', width: '100%' }}/>
+              <img src={login} style={{ height: '100%', width: '100%' }} />
             </div>
             <div className="w-full md:w-1/2 py-10 px-5 md:px-10">
               <form>
@@ -22,7 +23,7 @@ export default function Login() {
                       <label htmlFor="email" className="text-xs font-semibold px-1">Email</label>
                       <div className="flex">
                         <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
-                        <input type="email" id="email" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="johnsmith@example.com" />
+                        <input type="email" id="email" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus-border-indigo-500" placeholder="johnsmith@example.com" />
                       </div>
                     </div>
                   </div>
@@ -31,7 +32,7 @@ export default function Login() {
                       <label htmlFor="password" className="text-xs font-semibold px-1">Password</label>
                       <div className="flex">
                         <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
-                        <input type="password" id="password" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="************" />
+                        <input type="password" id="password" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus-border-indigo-500" placeholder="************" />
                       </div>
                     </div>
                   </div>
@@ -40,6 +41,9 @@ export default function Login() {
                       <button className="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">LOGIN</button>
                     </div>
                   </div>
+                  <p className="text-center text-gray-500">
+                    Don't have an account? <Link to="/register" className="text-indigo-600">Register here</Link>
+                  </p>
                 </div>
               </form>
             </div>
