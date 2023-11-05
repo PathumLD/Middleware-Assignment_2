69657f8bd46e9d8b3fdb68d5ebdd21e3b29d51f0
michellenikeetha <michellenikeetha@gmail.com>
2023-11-05 18:56:58 +0530

    change password backend by michelle

--- Backend/server.js
+++ Backend/server.js
@@ -31,15 +31,10 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
     const sql = "SELECT * FROM register WHERE `email` = ? AND `password` = ? ";
-    const values = [
-        req.body.name,
-        req.body.phone,
-        req.body.email,
-        req.body.password
-    ]
+
     db.query(sql, [req.body.email, req.body.password], (err, data) => {
         if (err) {
-            return res.json("Error");
+            return res.json("Error"); 
         }
         if(data.length > 0) {
             return res.json("Success");
@@ -47,6 +42,45 @@ app.post('/login', (req, res) => {
             return res.json("Fail");
         }
     })
+});
+
+app.put('/changepassword', (req, res) => {
+    
+    //retrieve the user from the database based on their email
+    const selectUserSQL = "SELECT * FROM register WHERE email = ?";
+    db.query(selectUserSQL, [req.body.email], (selectUserErr, userData) => {
+        if (selectUserErr) {
+            console.error("Error retrieving user data:", selectUserErr);
+            return res.json("Error");
+        }
+        if (userData.length === 0) {
+            console.error("User not found");
+            return res.json("user not found");
+        }
+
+        const user = userData[0];
+        // For debugging, display user.password in the console
+        console.log("User's Password:", user.password);
+
+        // Check if the current password matches the one stored in the database
+        if (user.password !== req.body.currentPassword) {
+            console.error("Current password is incorrect");
+            return res.json('Current password is incorrect' );
+        }
+
+        // Update the password to the new one
+        const sql = "UPDATE register SET `password`= ? WHERE email = ? ";
+        db.query(sql, [req.body.newPassword, req.body.email], (err, updatedData) => {
+            if (err) {
+                console.error("Failed to update password:", err);
+                return res.json("Failed to update password");
+            } else {
+                console.log("Password updated successfully");
+                return res.json("Updated Password Successfully!");
+            }
+        });
+    });
+
 });
 
 app.listen(8081, ()=> {
