64c3a0c1e4973f0e8785c9017cbfd639d57e4076
michellenikeetha <michellenikeetha@gmail.com>
2023-11-05 19:47:08 +0530

    services changes by michelle

--- frontend/src/components/services.jsx
+++ frontend/src/components/services.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
-import logo from '../assets/logo.png';
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import logo from "../assets/logo.png";
 
-export default function services() {
+export default function Services() {
+  // Sample telecommunication service packages data
+  const packages = [
+    { id: 1, name: "International Roaming", description: "Stay connected worldwide", usage: "Roaming", price: 499.00 },
+    { id: 2, name: "Data Unlimited", description: "Unlimited data for streaming and browsing", usage: "Data", price: 999.00 },
+    { id: 3, name: "Voice Plus", description: "Enhanced voice calling experience", usage: "Voice", price: 100.00 },
+    // Add more packages as needed
+  ];
+
   return (
-    <div className="w-[86%]" style={{ marginRight: "20px", marginLeft: "7%",  textAlign: "center" }}>
+    <div className="w-[86%]" style={{ marginRight: "20px", marginLeft: "7%" }}>
       {/* Logo and Navigation Container */}
       <div className="flex justify-between items-center mb-6">
         <div>
@@ -17,44 +25,23 @@ export default function services() {
         </Link>
       </div>
 
-      <div className="">
-        <div className="text-4xl mr-[70%] ">
+      <div className="text-center">
+        <div className="text-4xl">
           <h4>Telco Services Details</h4>
         </div>
 
-
-        <div className="pt-12 pb-6 ml-[77%] w-[20%] mt-[3%]">
+        <div className="pt-12 pb-6">
           <form>
             <div className="relative">
-              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none ml-[-24%]">
-                <svg
-                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg>
-              </div>
               <input
                 type="text"
-                className="block w-[130%] ml-[-25%] p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
-                placeholder="Search by name,reason"
+                className="block w-full p-3 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                placeholder="Search by name or reason"
                 required
               />
               <button
                 type="submit"
-                className="mr-[-5%] text-black absolute right-2.5 w-18 bg-gray-200 hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-xs px-3 py-2 "
-                style={{
-                  marginTop: -39,
-                }}
+                className="absolute right-2.5 top-2.5 w-18 bg-gray-200 hover-bg-gray-400 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-xs px-3 py-2"
               >
                 Search
               </button>
@@ -62,12 +49,15 @@ export default function services() {
           </form>
         </div>
 
-        <div className="relative overflow-x-auto shadow-md sm:rounded-b-lg ml-8 mt-2">
-          <table className="w-full text-sm text-left text-gray-500  ">
-            <thead className="text-xs text-[#374151] uppercase bg-gray-50 ">
+        <div className="overflow-x-auto shadow-md rounded-lg">
+          <table className="w-full text-sm text-left text-gray-500">
+            <thead className="text-xs text-[#374151] uppercase bg-gray-50">
               <tr>
+              <th scope="col" className="px-6 py-3">
+                  Id
+                </th>
                 <th scope="col" className="px-6 py-3">
-                  ServiceS
+                  Service
                 </th>
                 <th scope="col" className="px-6 py-3">
                   Description
@@ -76,275 +66,40 @@ export default function services() {
                   Usage
                 </th>
                 <th scope="col" className="px-6 py-3">
+                  Price
+                </th>
+                <th scope="col" className="px-6 py-3">
                   Action
                 </th>
               </tr>
             </thead>
-            <tbody className="">
-              <tr className="bg-white border-b">
-                <th className="px-6 py-4 text-gray-900  whitespace-nowrap dark:text-white ml-[10%]">
-                  1
-                </th>
-                <td className="px-6 py-4 "> International</td>
-                <td className="px-6 py-4 ">Roaming</td>
+            <tbody>
+              {packages.map((pkg) => (
+                <tr key={pkg.id} className="bg-white border-b">
+                  <th className="px-6 py-4 text-gray-900 whitespace-nowrap">{pkg.id}</th>
+                  <td className="px-6 py-4">{pkg.name}</td>
+                  <td className="px-6 py-4">{pkg.description}</td>
+                  <td className="px-6 py-4">{pkg.usage}</td>
+                  <td className="px-6 py-4">Rs.{pkg.price.toFixed(2)}</td>
 
-                <td className="px-4 py-4">
-                  <div className="">
-                    <button
-                      type="button"
-                      className="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 
-                    rounded-md px-2 py-2.5 inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
-                      style={{
-                        height: 30,
-
-                        marginTop: "0%",
-                        backgroundColor: "#363062",
-                      }}
-                    >
-                      <div
-                        className=""
-                        style={{
-                          fontSize: 12,
-                        }}
+                  <td className="px-4 py-4">
+                    <div className="space-x-4">
+                      <button
+                        type="button"
+                        className="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-md px-3 py-2.5 bg-blue-600 hover-bg-blue-700"
                       >
                         Activate
-                      </div>
-                    </button>
-
-                    <button
-                      type="button"
-                      className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-md text-medium px-2 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
-                      style={{
-                        height: 30,
-                        marginTop: "-6%",
-                        marginLeft: 40,
-                        paddingRight: 20,
-                      }}
-                    >
-                      <div
-                        style={{
-                          marginLeft: "11%",
-                          fontSize: "12px",
-                        }}
+                      </button>
+                      <button
+                        type="button"
+                        className="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-md px-3 py-2.5 bg-red-700 hover-bg-red-800"
                       >
                         Deactivate
-                      </div>
-                    </button>
-                  </div>
-                </td>
-              </tr>
-
-              <tr className="bg-white border-b">
-                <th className="px-6 py-4 text-gray-900  whitespace-nowrap dark:text-white ml-[10%]">
-                  2
-                </th>
-                <td className="px-6 py-4 "> Roaming Service</td>
-                <td className="px-6 py-4 ">Roaming</td>
-
-                <td className="px-4 py-4">
-                  <div className="">
-                    <button
-                      type="button"
-                      className="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 
-                    rounded-md px-2 py-2.5 inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
-                      style={{
-                        height: 30,
-
-                        marginTop: "0%",
-                        backgroundColor: "#363062",
-                      }}
-                    >
-                      <div
-                        className=""
-                        style={{
-                          fontSize: 12,
-                        }}
-                      >
-                        Activate
-                      </div>
-                    </button>
-
-                    <button
-                      type="button"
-                      className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-md text-medium px-2 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
-                      style={{
-                        height: 30,
-                        marginTop: "-6%",
-                        marginLeft: 40,
-                        paddingRight: 20,
-                      }}
-                    >
-                      <div
-                        style={{
-                          marginLeft: "11%",
-                          fontSize: "12px",
-                        }}
-                      >
-                        Deactivate
-                      </div>
-                    </button>
-                  </div>
-                </td>
-              </tr>
-
-              <tr className="bg-white border-b">
-                <th className="px-6 py-4 text-gray-900  whitespace-nowrap dark:text-white ml-[10%]">
-                  3
-                </th>
-                <td className="px-6 py-4 "> Ring-in Tone</td>
-                <td className="px-6 py-4 ">Personalization</td>
-
-                <td className="px-4 py-4">
-                  <div className="">
-                    <button
-                      type="button"
-                      className="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 
-                    rounded-md px-2 py-2.5 inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
-                      style={{
-                        height: 30,
-
-                        marginTop: "0%",
-                        backgroundColor: "#363062",
-                      }}
-                    >
-                      <div
-                        className=""
-                        style={{
-                          fontSize: 12,
-                        }}
-                      >
-                        Activate
-                      </div>
-                    </button>
-
-                    <button
-                      type="button"
-                      className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-md text-medium px-2 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
-                      style={{
-                        height: 30,
-                        marginTop: "-6%",
-                        marginLeft: 40,
-                        paddingRight: 20,
-                      }}
-                    >
-                      <div
-                        style={{
-                          marginLeft: "11%",
-                          fontSize: "12px",
-                        }}
-                      >
-                        Deactivate
-                      </div>
-                    </button>
-                  </div>
-                </td>
-              </tr>
-
-              <tr className="bg-white border-b">
-                <th className="px-6 py-4 text-gray-900  whitespace-nowrap dark:text-white ml-[10%]">
-                  4
-                </th>
-                <td className="px-6 py-4 "> Data Top-ups</td>
-                <td className="px-6 py-4 ">Personalization</td>
-
-                <td className="px-4 py-4">
-                  <div className="">
-                    <button
-                      type="button"
-                      className="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 
-                    rounded-md px-2 py-2.5 inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
-                      style={{
-                        height: 30,
-
-                        marginTop: "0%",
-                        backgroundColor: "#363062",
-                      }}
-                    >
-                      <div
-                        className=""
-                        style={{
-                          fontSize: 12,
-                        }}
-                      >
-                        Activate
-                      </div>
-                    </button>
-
-                    <button
-                      type="button"
-                      className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-md text-medium px-2 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
-                      style={{
-                        height: 30,
-                        marginTop: "-6%",
-                        marginLeft: 40,
-                        paddingRight: 20,
-                      }}
-                    >
-                      <div
-                        style={{
-                          marginLeft: "11%",
-                          fontSize: "12px",
-                        }}
-                      >
-                        Deactivate
-                      </div>
-                    </button>
-                  </div>
-                </td>
-              </tr>
-
-              <tr className="bg-white border-b">
-                <th className="px-6 py-4 text-gray-900  whitespace-nowrap dark:text-white ml-[10%]">
-                  5
-                </th>
-                <td className="px-6 py-4 "> Other VAS</td>
-                <td className="px-6 py-4 ">Services</td>
-
-                <td className="px-4 py-4">
-                  <div className="">
-                    <button
-                      type="button"
-                      className="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 
-                    rounded-md px-2 py-2.5 inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
-                      style={{
-                        height: 30,
-
-                        marginTop: "0%",
-                        backgroundColor: "#363062",
-                      }}
-                    >
-                      <div
-                        className=""
-                        style={{
-                          fontSize: 12,
-                        }}
-                      >
-                        Activate
-                      </div>
-                    </button>
-
-                    <button
-                      type="button"
-                      className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-md text-medium px-2 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
-                      style={{
-                        height: 30,
-                        marginTop: "-6%",
-                        marginLeft: 40,
-                        paddingRight: 20,
-                      }}
-                    >
-                      <div
-                        style={{
-                          marginLeft: "11%",
-                          fontSize: "12px",
-                        }}
-                      >
-                        Deactivate
-                      </div>
-                    </button>
-                  </div>
-                </td>
-              </tr>
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
