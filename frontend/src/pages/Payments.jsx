41cfc9335ecad3aa8cd05c74167c1811cd7a1694
PathumLD <2020is031@stu.ucsc.cmb.ac.lk>
2023-11-05 14:14:35 +0530

    Cart Items

--- frontend/src/pages/Payments.jsx
+++ frontend/src/pages/Payments.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Sidebar from '../common/Sidebar'
+
 import CreditCard from '../components/CreditCard'
-import Navbar from '../common/NavBar'
+
 
 const Payments = () => {
   return (
