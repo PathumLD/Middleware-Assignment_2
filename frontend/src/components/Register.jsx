b6314052c17ced8a3a47bb327db3797a94e317ff
michellenikeetha <michellenikeetha@gmail.com>
2023-11-05 12:33:08 +0530

    register validation by michelle

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import register from '../assets/register.jpg';
 import logo from '../assets/logo.png';
+import Validation from './RegisterValidation';
 
 export default function Register() {
+
+  const [values, setValues] = useState({
+    name: '',
+    phone: '',
+    email: '',
+    password: ''
+  })
+
+  const [errors, setErrors] = useState({})
+
+  const handleInput = (event) => {
+    setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors(Validation(values));
+  }
+
   return (
     <div className="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center px-5 py-5 relative">
       <img src={logo} alt="Logo" className="w-36 h-36 absolute top-5 left-5" /> {/* Logo here */}
@@ -13,26 +33,23 @@ export default function Register() {
             <img src={register} style={{ height: '100%', width: '100%' }} />
           </div>
           <div className="w-full md:w-1/2 py-10 px-5 md:px-10">
-            <form action="">
+            <form action="" onSubmit={handleSubmit}>
               <div className="text-center mb-10">
                 <h1 className="font-bold text-3xl text-gray-900">REGISTER</h1>
                 <p>Enter your information to register</p>
               </div>
               <div>
                 <div className="flex -mx-3">
-                  <div className="w-1/2 px-3 mb-5">
+                  <div className="w-full px-3 mb-5">
                     <label htmlFor="firstName" className="text-xs font-semibold px-1">First name</label>
                     <div className="flex">
                       <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                      <input type="text" id="firstName" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="John" />
+                      <input type="text" id="firstName" name='name'
+                        onChange={handleInput}
+                        className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" 
+                        placeholder="John Smith" />
                     </div>
-                  </div>
-                  <div className="w-1/2 px-3 mb-5">
-                    <label htmlFor="lastName" className="text-xs font-semibold px-1">Last name</label>
-                    <div className="flex">
-                      <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                      <input type="text" id="lastName" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Smith" />
-                    </div>
+                    {errors.name && <span className='text-red-600'>{errors.name}</span>}
                   </div>
                 </div>
                 <div className="flex -mx-3">
@@ -40,8 +57,12 @@ export default function Register() {
                     <label htmlFor="phone" className="text-xs font-semibold px-1">Mobile Phone</label>
                     <div className="flex">
                       <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
-                      <input type="tel" id="tel" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="07x xxxx xxx" />
+                      <input type="tel" id="phone" name='phone'
+                        onChange={handleInput}
+                        className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" 
+                        placeholder="07x xxxx xxx" />
                     </div>
+                    {errors.phone && <span className='text-red-600'>{errors.phone}</span>}
                   </div>
                 </div>
                 <div className="flex -mx-3">
@@ -49,8 +70,12 @@ export default function Register() {
                     <label htmlFor="email" className="text-xs font-semibold px-1">Email</label>
                     <div className="flex">
                       <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
-                      <input type="email" id="email" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="johnsmith@example.com" />
+                      <input type="email" id="email" name='email'
+                        onChange={handleInput}
+                        className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" 
+                        placeholder="johnsmith@example.com" />
                     </div>
+                    {errors.email && <span className='text-red-600'>{errors.email}</span>}
                   </div>
                 </div>
                 <div className="flex -mx-3">
@@ -58,13 +83,17 @@ export default function Register() {
                     <label htmlFor="password" className="text-xs font-semibold px-1">Password</label>
                     <div className="flex">
                       <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
-                      <input type="password" id="password" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="************" />
+                      <input type="password" id="password" name='password'
+                        onChange={handleInput}
+                        className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" 
+                        placeholder="************" />
                     </div>
+                    {errors.password && <span className='text-red-600'>{errors.password}</span>}
                   </div>
                 </div>
                 <div className="flex -mx-3">
                   <div className="w-full px-3 mb-5">
-                    <button className="block w-full max-w-xs mx-auto bg-indigo-500 hover-bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">REGISTER NOW</button>
+                    <button type="submit" className="block w-full max-w-xs mx-auto bg-indigo-500 hover-bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">REGISTER NOW</button>
                   </div>
                 </div>
                 <p className="text-center text-gray-500">
